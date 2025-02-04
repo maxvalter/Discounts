0a68ca69692a29e0f1c77ae943e451193244b400
maxvalter <43981565+maxvalter@users.noreply.github.com>
2025-02-04 19:53:04 +0100

    Updated xpaths

--- hemkop.py
+++ hemkop.py
@@ -1,18 +1,19 @@
 import time
 import datetime
 from sympy import product
-time_start = time.time()
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import csv
 
 from selenium.webdriver.firefox.options import Options
 
+time_start = time.time()
 options = Options()
 options.headless = False
 
-driver = webdriver.Firefox(options=options, executable_path='drivers/geckodriver')
+driver = webdriver.Firefox(options=options)
 driver.get('https://www.hemkop.se/erbjudanden')
 
 weeknumber = datetime.date.today().isocalendar()[1]
@@ -20,27 +21,22 @@ output = open('output/hemkop_promos' + '_w' + str(weeknumber) + '.csv', 'w')
 writer = csv.writer(output)
 
 time.sleep(4)
-cookietrust = driver.find_element_by_id('onetrust-reject-all-handler')
+cookie_xpath = '//*[@id="onetrust-reject-all-handler"]'
+cookietrust = driver.find_element(By.XPATH, cookie_xpath)
 cookietrust.click()
-
-#------
-#Välj butik
-
-#Klicka på box
-time.sleep(2)
-storeselect = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[4]/div[3]/form/div[2]')
-storeselect.click()
 
 time.sleep(2)
 
 #Skriv i box
-storesearch = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[4]/div[3]/form/div[2]/div/input')
+storesearch_xpath = '//*[@id="__next"]/div[2]/div[2]/div/div[2]/div/form/div/div/div[2]/input'
+storesearch = driver.find_element(By.XPATH, storesearch_xpath)
 storesearch.send_keys('Göteborg Vasagatan')
 
 #Klicka på butik
 time.sleep(1)
 # resultmenu = driver.find_element_by_class_name('store-select-header') #Elementet innan butiklänken
-store_element = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[4]/div[3]/form/div[2]/div/div[2]/ul/li[1]')
+store_element_xpath = '/html/body/div[1]/div[2]/div[2]/div/div[2]/div/form/div/div/div[3]/ul/li'
+store_element = driver.find_element(By.XPATH, store_element_xpath)
 store_element.click()
 
 time.sleep(2)
@@ -56,29 +52,23 @@ time.sleep(1)
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 time.sleep(3)
-grid_xpath = '/html/body/div[1]/div[1]/div[6]/div[1]'
+grid_xpath = '/html/body/div[1]/div[2]/div[4]/div[1]'
 
-grid = driver.find_element_by_xpath(grid_xpath)
-product_elements = grid.find_elements_by_xpath('./*')
+grid = driver.find_element(By.XPATH, grid_xpath)
+product_elements = grid.find_elements(By.XPATH, './*')
 
 
-print(len(product_elements))
-
-#Xpaths relative to product
-info_xpath = './div/div'
-# title_xpath = './div/div/div[4]/div[2]/div/div'
 
 #relative to infopath
-title_xpath = './div[4]/div[2]/div/div'
-price_xpath = './div[5]'
-desc_xpath = './div[4]/div[3]/p'
+title_xpath = './/*[@data-testid="product-title"][1]'
+price_xpath = './/*[contains(@data-testid, "splash")]'
+desc_xpath = title_xpath + '/../div'
 # compareprice_xpath = './ax-product-puff/div/div[3]/div/ax-product-pricelabel/div/div[1]/span[1]'
 
 for i in product_elements:
-    info_elem = i.find_element_by_xpath(info_xpath)
-    title_elem = info_elem.find_element_by_xpath(title_xpath)
-    price_elem = info_elem.find_element_by_xpath(price_xpath)
-    desc_elem = info_elem.find_element_by_xpath(desc_xpath)
+    title_elem = i.find_element(By.XPATH, title_xpath)
+    price_elem = i.find_element(By.XPATH, price_xpath)
+    desc_elem = i.find_element(By.XPATH, desc_xpath)
     
     product_data = [title_elem.text, price_elem.text, desc_elem.text]
     writer.writerow(product_data)
--- willys.py
+++ willys.py
@@ -1,7 +1,9 @@
 import time
 time_start = time.time()
 
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
@@ -13,40 +15,41 @@ import datetime
 options = Options()
 options.headless = False
 
-driver = webdriver.Firefox(options=options, executable_path='drivers/geckodriver')
+driver = webdriver.Firefox(options=options)
 # driver_path = '/usr/bin/safaridriver'
 
 driver.get("https://www.willys.se/erbjudanden/butik")
 #print(driver.title)
 
-weeknumber = datetime.date.today().isocalendar()[1]
-output = open('output/willys_promos' + '_w' + str(weeknumber) + '.csv', 'w')
-writer = csv.writer(output)
-
 time.sleep(3)
 
-cookietrust = driver.find_element_by_id("onetrust-reject-all-handler")
+#cookietrust = driver.find_element_by_id("onetrust-reject-all-handler")
+cookietrust = driver.find_element(By.XPATH, '//*[@id="onetrust-reject-all-handler"]')
 cookietrust.click()
 
-searchstores = driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/main/section/div[2]/div[2]/div/div/div/input')
+time.sleep(2)
+choosestore = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/main/section/div[1]/div/div[1]/div[1]/div/div[2]/p[3]/button')
+choosestore.click()
+
+searchstores = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/main/section/div[1]/div/div[1]/div/div/div/div/div/div[1]/div/input')
 searchstores.send_keys('Johanneberg')
 
 time.sleep(1)
 
 #store = driver.find_element_by_xpath('//*[@id="__next"]/div/div[3]/main/section/div[2]/div[2]/div[1]/div[2]/div/div[2]/div/ul/li/div/div[1]')
-store = driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/main/section/div[2]/div[2]/div/div/div[2]/div/ul')
+store = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/main/section/div[1]/div/div[1]/div/div/div/div/div/div[2]/div/ul/li[1]')
 store.click()
 
 time.sleep(2)
 
-showmore = driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/main/section/div[2]/div[3]/div/div/div/div[4]/div/button')
+showmore = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/main/section/div[1]/div/div[2]/div/div/div[3]/div/button')
 showmore.click()
 
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
 time.sleep(4)
 
-grid_xpath = '/html/body/div[1]/div/div[3]/main/section/div[2]/div[3]/div/div/div/div[3]'
+grid_xpath = '/html/body/div[1]/div/div[4]/main/section/div[1]/div/div[2]/div/div/div[2]'
 
 
 #Relative to grid_xpath
@@ -66,40 +69,31 @@ unit_xpath = './div/div/div/span[2]'
 multiples_xpath = './div[1]'
 
 
-product_grid = driver.find_element_by_xpath(grid_xpath)
-product_elements = product_grid.find_elements_by_xpath(product_xpath)
+product_grid = driver.find_element(By.XPATH, grid_xpath)
+product_elements = product_grid.find_elements(By.XPATH, product_xpath)
+
+weeknumber = datetime.date.today().isocalendar()[1]
+output = open('output/willys_promos' + '_w' + str(weeknumber) + '.csv', 'w')
+writer = csv.writer(output)
 
 for i in range(len(product_elements)-1):
     element = product_elements[i+1] #Skips first, not a product
 
-    title_element = element.find_element_by_xpath(title_xpath)
+    title_element = element.find_element(By.XPATH, title_xpath)
 
-    desc_element = element.find_element_by_xpath(desc_xpath)
+    desc_element = element.find_element(By.XPATH, desc_xpath)
 
 
-    price_elements = element.find_elements_by_xpath(price_children_xpath)
+    price_elements = element.find_elements(By.XPATH, price_children_xpath)
     price_info = ""
     for child in price_elements:
         price_info = price_info + child.text + ' '
-
-
-    # kronor_element = price_element.find_element_by_xpath(kronor_xpath)
-    # öre_element = price_element.find_element_by_xpath(öre_xpath)
-    # unit_element = price_element.find_element_by_xpath(unit_xpath)
 
     product_data = [title_element.text, price_info.replace('\n', ' '), desc_element.text.replace('\n', ' ')]
     writer.writerow(product_data)
 
     print('\n' + title_element.text + ": " + repr(price_info.replace('\n', ' ')) + ' ' 
      + repr(desc_element.text))
-
-# element = product_elements[1]
-# title_element = element.find_element_by_xpath(title_xpath)
-# price_element = element.find_element_by_xpath(price_xpath)
-# kronor_element = price_element.find_element_by_xpath(kronor_xpath)
-# öre_element = price_element.find_element_by_xpath(öre_xpath)
-
-# print(title_element.text + ": " + kronor_element.text + ',' + öre_element.text)
 
 driver.quit()
 
